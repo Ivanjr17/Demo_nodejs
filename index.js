0875aaae5cdb71e9c1c8d24b6c0c3d7dad9afdd8
ealvarez <ealvarez@ni.thedataage.com>
2023-03-11 12:07:07 -0600

    segundo commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { response } = require("express");
-const express = require("express");
-const send = require("send");
+const { response } = require('express')
+const express = require('express')
+const send = require('send')
 
-const app=express();
+const app = express()
 
-const PORT=4000;
+const PORT = 4000
 
-app.get("/", (req,res)=>{
-    res.send("Hola");
+app.get('/', (req, res) => {
+  res.send('Hola')
 })
 
-app.listen(PORT,()=>{
-    console.log("Aplicacion corriendo en puerto", PORT);
+app.listen(PORT, () => {
+  console.log('Aplicacion corriendo en puerto', PORT)
 })
